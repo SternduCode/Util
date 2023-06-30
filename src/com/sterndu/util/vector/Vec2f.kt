34478e808447157e9be5b7612d33152877474e7d
Sterndu <Sterndu13570@gmail.com>
2023-06-30 03:24:53 +0200

    Fixed some SonarLint warnings

--- src/com/sterndu/util/vector/Vec2f.kt
+++ src/com/sterndu/util/vector/Vec2f.kt
@@ -1,7 +1,6 @@
 @file:JvmName("Vec2f")
 package com.sterndu.util.vector
 
-import java.util.Vector
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -10,12 +9,10 @@ data class Vec2f @JvmOverloads constructor(val x: Float = 0.0f, val y: Float = 0
 	constructor(vector: Vec2f) : this(vector.x, vector.y)
 
 	fun distance(vector: Vec2f): Float {
- 		var dist = 0f
 		var xdist = x - vector.x
 		var ydist = y - vector.y
 		if (xdist < 0) xdist *= -1f
 		if (ydist < 0) ydist *= -1f
-		dist = sqrt(xdist.toDouble().pow(2.0) + ydist.toDouble().pow(2.0)).toFloat()
-		return dist
+		return sqrt(xdist.toDouble().pow(2.0) + ydist.toDouble().pow(2.0)).toFloat()
 	}
 }
