ba72810481edd41512a65eb9f8643db78f89fb7b
Sterndu <sterndu13570@gmail.com>
2025-12-14 17:54:44 +0100

    Set duplicatesStrategy to WARN for many Jar tasks
    Added `duplicatesStrategy = DuplicatesStrategy.WARN` to Jar tasks across multiple modules to ensure warnings are displayed for duplicate files. This helps prevent potential issues with packaging while maintaining awareness of file conflicts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ kotlin {
 	}
 }
 
+tasks.withType<Jar> {
+	duplicatesStrategy = DuplicatesStrategy.WARN
+}
+
 tasks.named("compileJava", JavaCompile::class.java) {
 	options.compilerArgumentProviders.add(CommandLineArgumentProvider {
 		// Provide compiled Kotlin classes to javac – needed for Java/Kotlin mixed sources to work
